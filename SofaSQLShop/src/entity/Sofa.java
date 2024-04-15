7eae51ecaba1d5f258dc966135d2d61930a62969
Darina Krostsenko <132041028+darlinz@users.noreply.github.com>
2024-04-15 13:34:06 +0300

    Update Sofa.java

--- SofaSQLShop/src/entity/Sofa.java
+++ SofaSQLShop/src/entity/Sofa.java
@@ -13,10 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author kristiinaparri
- */
+
 @Entity
 @Table(name="sofas")
 public class Sofa implements Serializable {
